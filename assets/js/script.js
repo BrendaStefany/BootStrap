35cca8c73406acbfe7ae8baf541c53ace1876525
BrendaStefany <brenda.stefany.cavalcanti@gmail.com>
2021-11-29 19:19:23 -0300

    [@BrendaStefany - Site Cactos]

---
+++ assets/js/script.js
@@ -0,0 +1,52 @@
+let nome = window.document.getElementById('nome')
+let email = document.querySelector('#email')
+let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+
+function validaNome() {
+    let txt = document.querySelector('#txtNome')
+    if (nome.value.length < 3) {
+        txt.innerHTML = 'Nome Inválido'
+        txt.style.color = 'red'
+    } else {
+        txt.innerHTML = 'Nome Válido'
+        txt.style.color = 'green'
+        nomeOk = true
+    }
+
+}
+
+
+function validaEmail() {
+    let txtEmail = document.querySelector('#txtEmail')
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+        txtEmail.innerHTML = 'Email Inválido'
+        txtEmail.style.color = 'red'
+    } else {
+        txtEmail.innerHTML = 'Email Válido'
+        txtEmail.style.color = 'green'
+        emailOk = true
+    }
+}
+
+function validaAssunto() {
+    let txtAssunto = document.querySelector('#txtAssunto')
+    if (assunto.value.length >= 100) {
+        txtAssunto.innerHTML = 'O texto digitado é muito grande, o máximo é 100 caracteres'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+    } else {
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
+
+function enviar() {
+    if (nomeOk == true && emailOk == true && assuntoOk == true) {
+        alert('Formulário enviado com sucesso!')
+    } else {
+        alert('Preencha os campos corretamente antes de enviar...')
+    }
+}
